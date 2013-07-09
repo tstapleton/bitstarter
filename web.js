8fee3b016dfac4c062e15ae4fea374a5a1212d89
Tom Stapleton <tstapleton@gmail.com>
2013-07-09 07:26:18 +0000

    Read from index.html

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  // response.send('Hello World 2!');
+  var s = fs.readFileSync('index.html', 'utf8');
+  response.send(s);
 });
 
 var port = process.env.PORT || 5000;
